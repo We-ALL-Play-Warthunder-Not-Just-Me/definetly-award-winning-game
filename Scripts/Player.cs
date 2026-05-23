a82856faa867af1e0975e0a0557c1d3dd25dc932
ValentineCats <pyrostheotherfiremage@gmail.com>
2026-05-22 22:12:16 -0400

    Walking and jumping
    It works.... for now

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,57 +1,169 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class Player : CharacterBody2D
 {
 
-	enum PlayerState
+	public enum PlayerMoveState
 	{
 		IDLE,
 		WALK,
-		JUMP,
-		CLIMB,
-		FALL,
-		LAND,
-		HURT
+		RUN,
+		DASH
+	}
+
+    public enum PlayerJumpState
+    {
+        NONE,
+        JUMPING,
+        FALLING,
+		LANDING
+    }
+    public enum PlayerState
+    {
+        HURT,
+		RECOVING,
+		FINE
+    }
+
+    public enum  PlayerAttackState
+    {
+		ATTACKING,    
+		RECOVERING,
+		IDLE
+    }
+
+    public enum EnvironmentalState
+	{
+		GROUNDED,
+		AIRBORN,
+		LIQUID,
+		WALL//Uncertain if this should be here
 	}
 
 
+	//Constants
 	public const float SPEED = 300.0f;
 	public const float JUMPVELOCITY = -200.0f;
 	public const float GRAVITY = 700.0f;
 
-
+	//player parts
 	public CharacterBody2D PlayerObject;
     public Sprite2D PlayerSprite;
     public CollisionShape2D PlayerCollisionShape;
 
+	public PlayerMoveState pms = PlayerMoveState.IDLE; 
+	public PlayerState ps = PlayerState.FINE;
+	public PlayerJumpState pjs = PlayerJumpState.FALLING;
+    public EnvironmentalState es = EnvironmentalState.AIRBORN;
+
     public override void _Ready()
     {
-        PlayerObject = GetNode<CharacterBody2D>("Player");
+		PlayerObject = this;
+			//GetNode<CharacterBody2D>("Player");
 		PlayerSprite = GetNode<Sprite2D>("PlayerSprite2D");
 		PlayerCollisionShape = GetNode<CollisionShape2D>("PlayerCollisionShape2D");
     }
 
-	private void _apply_gravity(double delta)
+	private void _apply_gravity(double delta, ref Vector2 velocity)
 	{
 		if (!PlayerObject.IsOnFloor())
 		{
-			Vector2 velocity = Velocity;
-            velocity += GetGravity() * (float)delta;
-            Velocity = velocity;
+            velocity.Y += GRAVITY * (float)delta;
         }
 	}
 
-	private void _process_state(double delta)
+
+    //returns true if state changes and false if state is the same as before, currently not implemented.
+    private bool _change_Jump_State(PlayerJumpState s, bool t)
 	{
-		
+		if (s == pjs)
+		{
+			return false;
+		}
+		switch(s)
+        {
+            case (PlayerJumpState.NONE):
+                pjs = s;
+                return true;
+            case (PlayerJumpState.JUMPING):
+                pjs = s;
+                return true;
+            case (PlayerJumpState.FALLING):
+                pjs = s;
+                return true;
+            case (PlayerJumpState.LANDING):
+                pjs = s;
+                return true;
+        }
+        return false;
+	}
+
+    private void _process_jump_state(double delta, ref Vector2 velocity)
+    {
+        if (Input.IsActionJustPressed("Jump") && IsOnFloor())
+        {
+            velocity.Y = JUMPVELOCITY;
+        }
+    }
+    //returns true if state changes and false if state is the same as before, currently not implemented.
+    private bool _change_Move_State(PlayerMoveState s, bool t)
+	{
+		if (s == pms)
+		{
+			return false;
+		}
+		switch (s)
+		{
+			case (PlayerMoveState.IDLE):
+				pms = s;
+				return true;
+			case(PlayerMoveState.WALK):
+                pms = s;
+				return true;
+		}
+		return false;
+	}
+	private void _process_move_state(double delta, ref Vector2 velocity)
+    {
+        Vector2 direction = Input.GetVector("Left", "Right", "ui_up", "ui_down");
+        if (direction != Vector2.Zero)
+        {
+            _change_Move_State(PlayerMoveState.WALK, true);
+            velocity.X = direction.X * SPEED;
+        }
+        else if (pms == PlayerMoveState.IDLE)
+        {
+			velocity.X = Mathf.MoveToward(velocity.X, 0, SPEED);
+        } 
+		else 
+		{
+            _change_Move_State(PlayerMoveState.IDLE, true);
+        }   
+    }
+
+    private void _process_environmental_state(double delta)
+	{
+		if (PlayerObject.IsOnFloor())
+		{
+			//_change_State(PlayerState.GROUNDED)
+		}
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		_apply_gravity(delta);
+        Vector2 velocity = Velocity;
+        _process_environmental_state(delta);
+		_process_move_state(delta, ref velocity);
+		_process_jump_state(delta, ref velocity);
+        _apply_gravity(delta, ref velocity);
+        
 
-		/*
+
+		Velocity = velocity;
+        MoveAndSlide();
+        /*
 		Vector2 velocity = Velocity;
 
 		// Add the gravity.
@@ -80,5 +192,5 @@ public partial class Player : CharacterBody2D
 
 		Velocity = velocity;
 		MoveAndSlide();*/
-	}
+    }
 }
