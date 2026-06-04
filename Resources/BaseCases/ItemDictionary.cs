0bb5c359dd116fe2977f7c94f165784915702b18
Snowyfreezer <99560297+Snowyfreezer@users.noreply.github.com>
2026-06-04 05:27:35 -0400

    renamed inventorydictionary to ItemDictionary

--- Resources/BaseCases/ItemDictionary.cs
+++ Resources/BaseCases/ItemDictionary.cs
@@ -2,13 +2,13 @@ using Godot;
 using System;
 
 [GlobalClass]
-public partial class InventoryDictionary : Resource
+public partial class ItemDictionary : Resource
 {
     [Export]
     public Godot.Collections.Dictionary<int,Resource> items = new Godot.Collections.Dictionary<int,Resource>();
 
 
-    public InventoryDictionary() {}
+    public ItemDictionary() {}
 
     
 }
