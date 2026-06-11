288520cb4200c7ac7954054ec56312932e25ea17
Snowyfreezer <99560297+Snowyfreezer@users.noreply.github.com>
2026-06-10 22:15:17 -0400

    added item pickup scene and test items to map 1

---
+++ Scripts/InventoryStuff/PickUpItem.cs
@@ -0,0 +1,65 @@
+using Godot;
+using System;
+
+public partial class PickUpItem : Area2D
+{
+
+	[Export] Item baseItem;
+	[Export] int amount;
+	Item item;
+	// Can be the Materials inventory itemlist or the KeyItems inventory Itemlist
+	[Export] InventoryLogic invent;
+	// [Export] bool MaterialInventory = true;
+	[Export] bool KeyItemInventory = false;
+	private Sprite2D sprite;
+	private Node globals;
+
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		BodyEntered += ItemTouched;
+		item = baseItem.shallowCopy(amount);
+		sprite = GetNode<Sprite2D>("Sprite2D");
+		sprite.Texture= baseItem.icon;
+		// invent = GetNode<InventoryLogic>("../../../PauseStatus/InventoryScreen/Panel/TabContainer/Inventory/TabContainer/Materials/MarginContainer/ItemList");
+		// invent = GetNode<InventoryLogic>("/root/GameScene/PauseStatus/InventoryScreen/Panel/TabContainer/Inventory/TabContainer/Materials/MarginContainer/ItemList");
+		// invent = GetNode<InventoryLogic>("/root/GameScene/PauseStatus/InventoryScreen/Panel/TabContainer/Inventory/TabContainer/KeyItems/MarginContainer/ItemList");		
+		// SendItemToMaterialInventory.Instance.EmitSignal(SignalName.SentItem,);
+		// if (!KeyItemInventory)
+		// {
+		// 	globals = GetNode("/root/SendItemtoMaterialInventory") as SendItemToMaterialInventory;
+		// 	// globals.EmitSignal(SendItemToMaterialInventory.SignalName.SentItem);			
+		// }
+		// else if (KeyItemInventory)
+		// {
+		// 	globals = GetNode("/root/SendItemtoKeyItemInventory") as SendItemToKeyItemInventory;
+		// }
+		if (!KeyItemInventory)
+		{
+			invent = GetNode<InventoryLogic>("/root/GameScene/PauseStatus/InventoryScreen/Panel/TabContainer/Inventory/TabContainer/Materials/MarginContainer/ItemList");
+		}
+		else if (KeyItemInventory)
+		{
+			invent = GetNode<InventoryLogic>("/root/GameScene/PauseStatus/InventoryScreen/Panel/TabContainer/Inventory/TabContainer/KeyItems/MarginContainer/ItemList");
+		}
+
+	}
+
+	private void ItemTouched(Node2D body)
+	{
+		if (body.IsInGroup("Player"))
+		{
+			if (AddNewItem())
+			{
+				QueueFree();
+			}
+		}
+	}
+
+	private bool AddNewItem()
+	{
+		return invent.AddInventoryItem(item);
+	}
+
+}
