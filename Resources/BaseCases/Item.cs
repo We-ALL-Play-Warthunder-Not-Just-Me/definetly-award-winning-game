9a94e8ab40d88655e8f14bc5b078919284a18235
Gadzik <oshawot123@gmail.com>
2026-06-13 01:58:50 -0500

    Fixing my ridiculous Z-Indexing
    Switched all of my Decorations form 6/8/0 to 2 so that they stay in front of the Player. I also moved the player from 5 to 1 as I'm sure I was the one who set it that high to begin with at some point.

--- Resources/BaseCases/Item.cs
+++ Resources/BaseCases/Item.cs
@@ -12,40 +12,40 @@ using System;
    [Export] public bool important = false;
    [Export] public bool consumable = true;
 
-    public Item() : this(0,null,0,0,null)
-    {
-        
-    }
+	public Item() : this(0,null,0,0,null)
+	{
+		
+	}
 
-    public Item(int ID, string name, int qty, int max_qty, Texture2D icon, bool important = false, bool consumable = true)
-    {
-        this.ID = ID;
-        this.Name = name;
-        this.qty = qty;
-        this.max_qty = max_qty;
-        this.icon = icon;
-        this.important = important;
-        this.consumable = consumable;
-    }
+	public Item(int ID, string name, int qty, int max_qty, Texture2D icon, bool important = false, bool consumable = true)
+	{
+		this.ID = ID;
+		this.Name = name;
+		this.qty = qty;
+		this.max_qty = max_qty;
+		this.icon = icon;
+		this.important = important;
+		this.consumable = consumable;
+	}
 
-    public Item shallowCopy()
-    {
-        return (Item)MemberwiseClone();
-    }
+	public Item shallowCopy()
+	{
+		return (Item)MemberwiseClone();
+	}
 
-    public Item shallowCopy(int newqty)
-    {
-        Item tempItem = (Item)MemberwiseClone();
-        tempItem.qty = newqty;
-        return tempItem;
-    }
+	public Item shallowCopy(int newqty)
+	{
+		Item tempItem = (Item)MemberwiseClone();
+		tempItem.qty = newqty;
+		return tempItem;
+	}
 
-    // I don't have an Object inside my Object
-    // public Item deepCopy()
-    // {
-    //     Item other = (Item)MemberwiseClone();
-    //     other.
-    //     return other;
-    // }
+	// I don't have an Object inside my Object
+	// public Item deepCopy()
+	// {
+	//     Item other = (Item)MemberwiseClone();
+	//     other.
+	//     return other;
+	// }
 
 }
